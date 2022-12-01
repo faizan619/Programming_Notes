e5aeac86859349bb93be73faad4c10cd82f4e9fc
faizan alam <95950378+faizan619@users.noreply.github.com>
2022-12-02 00:18:25 +0530

    Update 5]Loops_BreakContinue.dart

--- Dart programming/5]Loops_BreakContinue.dart
+++ Dart programming/5]Loops_BreakContinue.dart
@@ -8,4 +8,15 @@ void main(){
   for(int i=10;i>=1;i--){
     print("$i");
   }
+  //--printing number from 5 to 10...
+  for(int i=5;i<=10;i++){
+    //print("$i");
+  }
+  //nested loop in dart
+  for(int i=1;i<=5;i++){
+    for(int j=1;j<=5;j++){
+      print("$j");
+      //dont know how to make the loop continue without breakline..
+    }
+  }
 }
