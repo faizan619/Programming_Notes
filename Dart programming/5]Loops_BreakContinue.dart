d41ca3d897bc768e3d629f6e556129d55e43f189
faizan alam <95950378+faizan619@users.noreply.github.com>
2022-12-01 23:50:03 +0530

    Update 5]Loops_BreakContinue.dart

--- Dart programming/5]Loops_BreakContinue.dart
+++ Dart programming/5]Loops_BreakContinue.dart
@@ -1,5 +1,11 @@
-void main() {
-  for (int i = 0; i < 5; i++) {
-    print('hello ${i + 1}');
+void main(){
+  print("Loops in Dart");
+  //--printing number from 1 to 10...
+  for(int i=0;i<=10;i++){
+    print("$i");
+  }
+  //--printing number from 10 to 1...
+  for(int i=10;i>=1;i--){
+    print("$i");
   }
 }
