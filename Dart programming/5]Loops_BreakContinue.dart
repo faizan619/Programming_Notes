a14f9d9aac435facd83a313838897224fa99b8bf
faizan alam <95950378+faizan619@users.noreply.github.com>
2022-12-02 22:19:19 +0530

    Update 5]Loops_BreakContinue.dart

--- Dart programming/5]Loops_BreakContinue.dart
+++ Dart programming/5]Loops_BreakContinue.dart
@@ -19,4 +19,25 @@ void main(){
       //dont know how to make the loop continue without breakline..
     }
   }
+  
+  
+  //while loop in dart
+  //--printing number from 1 to 10...
+  var a1 = 1;
+  while(a1<=10){
+    print("while: $a1");
+    a1++;
+  }
+  //-printing number from 10 to 1...
+  print("value of a1 now is $a1");
+  while(a1>=1){
+    print("reverse while : $a1");
+    a1--;
+  }
+  //--printing number from 5 to 10...
+  a1 = 5;
+  while(a1<=10){
+    print("range 5-10 :  $a1");
+    a1++;
+  }
 }
